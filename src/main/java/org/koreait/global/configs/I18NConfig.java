145ebc95e460dad315a2afcc2d112709603ab9bd
juyonghyeon <jyh928578@gmail.com>
2025-05-27 15:43:12 +0900

    v2

---
+++ src/main/java/org/koreait/global/configs/I18NConfig.java
@@ -0,0 +1,10 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * I18N - Internationalizational
+ */
+@Configuration
+public class I18NConfig {
+}
