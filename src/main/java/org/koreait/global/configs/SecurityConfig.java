a405b8348d7a7991ee7d693cb0af0a233eb016e9
juyonghyeon <jyh928578@gmail.com>
2025-06-02 18:21:39 +0900

    6/02_저장

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(c -> {
             c.requestMatchers("/mypage/**").authenticated()
                     .requestMatchers("/member/join","/member/join").anonymous()
-                    .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+                    // .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
                     .anyRequest().permitAll();
         });
 
