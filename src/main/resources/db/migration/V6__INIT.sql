95db1ff41e9cc97aa85f8d2a8b9ca97a7806e6fa
juyonghyeon <jyh928578@gmail.com>
2025-07-01 00:08:32 +0900

    15

--- src/main/resources/db/migration/V6__INIT.sql
+++ src/main/resources/db/migration/V6__INIT.sql
@@ -2,12 +2,13 @@ CREATE TABLE FILE_INFO (
     seq BIGINT AUTO_INCREMENT,
     gid VARCHAR(45) NOT NULL,
     location VARCHAR(45),
-    filename VARCHAR(100) NOT NULL,
+    fileName VARCHAR(100) NOT NULL,
     extension VARCHAR(40) NOT NULL,
     contentType VARCHAR(60) NOT NULL,
+    done TINYINT(1) DEFAULT 0,
     createdBy VARCHAR(65),
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    modifiedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modifiedAt DATETIME,
     deletedAt DATETIME,
     PRIMARY KEY(seq)
 );
