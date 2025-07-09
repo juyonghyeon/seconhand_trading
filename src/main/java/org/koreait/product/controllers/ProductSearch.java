129db35db2a21a04ba11801a426357bf7295292d
juyonghyeon <jyh928578@gmail.com>
2025-07-09 22:30:03 +0900

    17

--- src/main/java/org/koreait/product/controllers/ProductSearch.java
+++ src/main/java/org/koreait/product/controllers/ProductSearch.java
@@ -2,11 +2,9 @@ package org.koreait.product.controllers;
 
 import lombok.Data;
 import org.koreait.global.search.CommonSearch;
-import org.koreait.product.constants.ProductStatus;
-
-import java.util.List;
 
 @Data
 public class ProductSearch extends CommonSearch {
-    private List<ProductStatus> productStatus;
+    private Integer sPrice;
+    private Integer ePrice;
 }
