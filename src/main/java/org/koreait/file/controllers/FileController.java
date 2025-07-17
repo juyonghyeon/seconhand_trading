e2a9d5f5634761dc6e01b4cb05c209b637e5e217
juyonghyeon <jyh928578@gmail.com>
2025-07-17 12:53:53 +0900

    .

--- src/main/java/org/koreait/file/controllers/FileController.java
+++ src/main/java/org/koreait/file/controllers/FileController.java
@@ -1,9 +1,19 @@
 package org.koreait.file.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.koreait.file.entities.FileInfo;
 import org.koreait.file.services.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +25,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping({"/api/file", "/file"})
+@Tag(name="파일 API", description = "파일 업로드, 다운로드, 조회기능을 제공")
 public class FileController {
     private final FileUploadService uploadService;
     private final FileDeleteService deleteService;
@@ -22,7 +33,10 @@ public class FileController {
     private final FileDownloadService downloadService;
     private final ThumbnailService thumbnailService;
 
+    @Operation(summary = "파일 업로드 처리", method = "MULTIPART")
+    @ApiResponse(responseCode = "201", description = "파일 업로드 성공시 업로드한 파일 목록이 출력")
     @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
     public List<FileInfo> upload(RequestUpload form, @RequestPart("file") MultipartFile[] files) {
         form.setFiles(files);
         List<FileInfo> items = uploadService.process(form);
@@ -38,6 +52,10 @@ public class FileController {
         return items;
     }
 
+    @Operation(summary = "파일 등록번호로 파일 정보 한개 조회")
+    @Parameters({
+            @Parameter(name="seq", in = ParameterIn.PATH, required = true, description = "파일 등록 번호")
+    })
     @GetMapping("/info/{seq}")
     public FileInfo info(Long seq) {
         FileInfo item = infoService.get(seq);
@@ -46,7 +64,7 @@ public class FileController {
     }
 
     @DeleteMapping("/delete/{seq}")
-    public FileInfo delete(Long seq) {
+    public FileInfo delete(@PathVariable("seq") Long seq) {
         FileInfo item = deleteService.process(seq);
 
         return item;
@@ -86,6 +104,24 @@ public class FileController {
             out.write(bis.readAllBytes());
 
         } catch (IOException e) {}
+    }
+
+    @GetMapping("/image/{seq}")
+    public ResponseEntity<byte[]> showImage(@PathVariable("seq") Long seq) {
+        FileInfo item = infoService.get(seq);
+
+        String contentType = item.getContentType();
+        byte[] bytes = null;
+        File file = new File(item.getFilePath());
+        try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+            bytes = bis.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf(contentType));
+
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
     }
 
 //    @GetMapping("/download/{seq}")
